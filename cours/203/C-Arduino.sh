1eba3708f1f163a7c25567eff329b34033c06d6f
NicHub <nicolas.jeanmonod@gmail.com>
2016-02-29 09:31:40 +0100

    Réorganisation des fichiers
    # Supression des scripts de génération des PDF ⇒ remplacés par
    `generation/md2pdf.sh`
    git rm cours/203/C-Arduino.sh
    git rm cours/204/enseignes-fixes.sh
    git rm cours/505/matrice-multiplex.sh
    
    # Supression des scripts de test
    git rm cours/404/gen-images-400.sh
    git rm generation/pre-dia-nouveau.py
    
    # Supression de `infos.yaml` pour la TOC
    git rm cours/000/infos.yaml
    
    # Création du répertoire `cours/_00/` et déplacement des fichiers de
    suivi de projet dans ce répertoire
    git mv cours/000/avance-pyr.md                 cours/_00/
    git mv cours/000/generic-dia-compact.pdf       cours/_00/
    git mv cours/000/generic-dia-compact.sh        cours/_00/
    git mv cours/000/generic-dia.md                cours/_00/
    git mv cours/000/generic-dia.pdf               cours/_00/
    git mv cours/000/generic-dia.sh                cours/_00/
    git mv cours/000/point-2016-02-13-compact.pdf  cours/_00/
    git mv cours/000/point-2016-02-13.md           cours/_00/
    git mv cours/000/point-2016-02-13.pdf          cours/_00/

--- cours/203/C-Arduino.sh
+++
@@ -1,2 +0,0 @@
- #!/bin/bash
-pandoc -s --normalize --css ../../statiques/style.css --mathjax -o C-Arduino.html C-Arduino.md
--- cours/204/enseignes-fixes.sh
+++
@@ -1,2 +0,0 @@
- #!/bin/bash
-pandoc -s --normalize --css ../../statiques/style.css --mathjax -o enseignes-fixes.html enseignes-fixes.md
